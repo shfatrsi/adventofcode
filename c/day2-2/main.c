cb05292a838ec38403e31d5339b694991b9311c7
steven.frost <steven.frost@ruralsourcing.com>
2019-12-07 23:45:07 +0000

    Adds new directory for day2-2 as copy of day 2-1

---
+++ c/day2-2/main.c
@@ -0,0 +1,202 @@
+/** Includes **/
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/** Defines **/
+#define NELEMS(x) (sizeof x / sizeof *x)
+
+/** Enums **/
+typedef enum {false, true} bool;
+
+/** Prototypes Declerations **/
+int* readCloseFile(FILE *file, int *arrSize);
+void readCodes(int *opCodes, int arrSize);
+int readFourCodes(int *fourCodes, int arrSize);
+int doTheMaths(int *fourCodes, int arrSize);
+void replaceCodeWithNewValue(int *opCodes, int index, int newValue);
+bool isOob(int num, int arrSize);
+bool isCode99(int code);
+bool isDigit(char c);
+
+/** Main **/
+int main()
+{
+     int arrSize = 0;
+     int *opCodes=readCloseFile(
+	  fopen("C:/input/day2-1",
+	  "r"),
+	  &arrSize);
+
+     readCodes(opCodes, arrSize);
+
+     for(int i=0; i<arrSize; i++)
+     {
+	  printf("%d\n", opCodes[i]);
+     }
+     printf("Value at 0: %d", opCodes[0]);
+
+     getchar();
+     return(0);
+}
+
+/** Prototype Implementations **/
+int* readCloseFile(FILE *file, int *arrSize)
+{
+     fseek(file, 0L, SEEK_END);
+     // File size now know
+     int fs = ftell(file);
+     rewind(file);
+     printf("File Size: %d\n", fs);
+
+     // Allocate
+     int *numArr = malloc(fs);
+     int endSize = NELEMS(numArr);
+     int arrSizeReal = 0;
+     printf("Number size: %d\n\n", numArr);     
+     
+     for(int i=0; feof(file) != 1; i++)
+     {
+	  char *buff = malloc(10);
+          fscanf(file, "%10[^,]%*c", buff);
+	  // TODO(shf): Look deeper into fscanf
+	  //  and what strtol does
+	  char *tmp;
+	  int number = strtol(buff, &tmp, 10);
+	
+	  if (numArr[i] == endSize)
+	  {
+	       // TODO(shf): Realocate array size if end
+	       //  reached. double check the numbers match
+	  }
+	  else
+	  {
+	       numArr[i] = number;
+	       arrSizeReal++;
+	  }
+	  free(buff);
+	  free(tmp);
+     }
+
+     printf("real array size: %d\n", arrSizeReal);
+     
+     // Assign file size, close file, and return ptr with
+     // number values
+     *arrSize = arrSizeReal;
+     // Close file and free ptrs
+     fclose(file);
+
+     return numArr;
+}
+
+void readCodes(int *opCodes, int arrSize)
+{
+     for(int i=0; i<arrSize; i+=4)
+     {
+	  bool code99 = isCode99(opCodes[i]);
+
+	  // Code 99 reached time to stop
+	  if (code99)
+	  {
+	       printf("code 99\n\n");
+	       return;
+	  }
+
+	  // This breaks the oob check
+	  int pos1 = opCodes[i+1];
+	  int pos2 = opCodes[i+2];
+
+	  int fourCodes[] = {opCodes[i],
+			     opCodes[pos1],
+			     opCodes[pos2],
+			     opCodes[i+3]};
+
+	  int value = readFourCodes(fourCodes, arrSize);
+
+	  if(value == -2)
+	  {
+	       // Out of bounds something is wrong
+	       return;
+	  }
+	  if (value != -1)
+	  {
+	       //printf("Value: %d", value);
+	       replaceCodeWithNewValue(opCodes, fourCodes[3], value);
+	  }
+	  printf("\n");
+     }
+     /*
+     for (int i=0; i<arrSize; i++)
+     {
+	  printf("%d\n", opCodes[i]);
+     }
+     printf("\nCode at pos 0: %d", opCodes[0]);*/
+}
+
+int readFourCodes(int *fourCodes, int arrSize)
+{
+     for(int i=0; i<4; i++)
+     {
+	  /*bool oob = isOob(fourCodes[i], arrSize);
+	  if (oob)
+	  {
+	       printf("Code %d is out of bounds!\n", fourCodes[i]);
+	       return(-2);
+	  }*/
+	  printf("code: %d\n", fourCodes[i]);
+     }
+     return(doTheMaths(fourCodes, arrSize));
+}
+
+int doTheMaths(int *fourCodes, int arrSize)
+{
+     int value = -1;
+
+     switch(fourCodes[0])
+     {
+     case 1:
+	  value = fourCodes[1] + fourCodes[2];
+	  break;
+     case 2:
+	  value = fourCodes[1] * fourCodes[2];
+	  break;
+     default:
+	  //printf("Unknown Value\n");
+	  break;
+     }
+
+     printf("Value: %d", value);
+     return(value);
+}
+
+void replaceCodeWithNewValue(int *opCodes, int index, int newValue)
+{
+     opCodes[index] = newValue;
+}
+
+bool isOob(int num, int arrSize)
+{
+     if(num >= arrSize)
+     {
+	  return true;
+     }
+     return false;
+}
+
+bool isCode99(int code)
+{
+     if(code==99)
+     {
+	  return true;
+     }
+     return false;
+}
+
+bool isDigit(char c)
+{
+     if(c >= '0' && c <='9')
+     {
+	  return true;
+     }
+     return false;
+}
